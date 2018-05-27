ac3c6e3b7ae0d86776f9a6fcbee9c1930ea33c78
zhuzhe <zhe.zhu1@pactera.com>
2018-05-27 16:06:25 +0800

    second consumer

---
+++ src/main/java/com/space/rabbitmq/receiver/SecondConsumer.java
@@ -0,0 +1,20 @@
+package com.space.rabbitmq.receiver;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 消息消费者2
+ * @author zhuzhe
+ * @date 2018/5/25 17:32
+ * @email 1529949535@qq.com
+ */
+@Component
+public class SecondConsumer {
+
+    @RabbitListener(queues = {"first-queue","second-queue"}, containerFactory = "rabbitListenerContainerFactory")
+    public void handleMessage(String message) throws Exception {
+        // 处理消息
+        System.out.println("SecondConsumer {} handleMessage :"+message);
+    }
+}
